590d92a00c9768e49a7a6f18228b379ae3d07f7a
Walker Le <walker.le88@gmail.com>
2023-02-22 22:00:40 -0700

    commit tree 1ec4afa8843a8638be1aab02facea085d5e8dcec

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,28 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :ok
+    else
+      render json: { error: "plant not found"}, status: :not_found
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "plant not found"}, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
